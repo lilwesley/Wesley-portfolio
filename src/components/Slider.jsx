6e7bfd371f8340cded87af6efd221095def075fd
lilwesley <wesley.yosafullerton@gmail.com>
2023-04-12 19:39:53 +0700

    react-app

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,55 +1,112 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
-import styled from "styled-components"
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
+import styled from "styled-components";
+import { data } from "../data";
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
 `;
 
 const Wrapper = styled.div`
-    height: 100%;
-`
-const Slide = styled.div`
-display: flex;
+  height: 100%;
+  display: flex;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
+  transition: all 1s ease
 `;
 
-const ImgContainer = styled.div``; (menit 34.34)
-const infoContainer = styled.div``;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 85%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+font-size: 70px;
+`;
+const Desc = styled.p`
+font-size: 40px;
+margin: 50px 0px;
+letter-spacing: 3px;
+font-weight: 600;
+`;
+const Button= styled.button`
+font-size: 20px;
+border: 1px solid black;
+padding: 5px;
+background: transparent;
+`;
 
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    background-color: #f3e8e8;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: ${props=> props.direction === "left" && "10px"};
-    right: ${props=> props.direction === "right" && "10px"};
-    margin: auto;
-    cursor:pointer;
-    opacity: 0.5;
+  width: 50px;
+  height: 50px;
+  background-color: #f3e8e8;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
 
 const Slider = () => {
+  const [slideIndex,setSlideIndex]= useState(0)
+    const handleClick = (direction) => {
+      if(direction === "left"){
+        setSlideIndex(slideIndex>0?slideIndex-1:0)
+      }
+      setSlideIndex(slideIndex<2?slideIndex+1:0)
+    };
   return (
     <Container>
-       <Arrow direction="left">
-            <ArrowLeftOutlined/>
-       </Arrow>
-       <Wrapper>
-
-       </Wrapper>
-       <Arrow direction="right">
-            <ArrowRightOutlined/>
-       </Arrow>
+      <Arrow direction="left" onClick={()=>handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+      {data.map((item)=> (
+        
+        <Slide>
+        <ImgContainer>
+        <Image src={item.img}/>
+        </ImgContainer>
+        <InfoContainer>
+        <Title>{item.title}</Title>
+        <Desc>{item.desc}</Desc>
+        <Button>Belanja Ngentot</Button>
+        </InfoContainer>
+        </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={()=>handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id:1,
+        title:"lorem", 
+        desc: "tes apa ge",
+        img: "https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+    },
+    {
+        id:2,
+        title: "lorem slow mo", 
+        desc: "NAHAB SUGAB",
+        img: "https://images.unsplash.com/photo-1613255348086-0f34a22398b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
+    },
+    {
+        id:3,
+        title: "lorem", 
+        desc: "tes apa ge",
+        img: "https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+    }
+]
